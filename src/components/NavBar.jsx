976c7a67cf211b0394a0ad9d4a33886848de6ff6
Mahin Ahmed <mahinahmed@Mahins-MacBook-Air.local>
2026-05-06 15:37:57 +0900

    fixed ham

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -24,7 +24,13 @@ export default function () {
                 aria-expanded="false"
                 aria-label="Toggle navigation"
             >
-                <span className="navbar-toggler-icon"></span>
+                <span
+                    className="navbar-toggler-icon"
+                    style={{
+                        backgroundImage:
+                            "url(\"data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%28255, 255, 255, 1%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e\")"
+                    }}
+                ></span>
             </button>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
