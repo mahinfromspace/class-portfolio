1ed0eab12b6389ba5aa36bde978ac9ee55d819c1
Mahin Ahmed <mahinahmed@Mahins-MacBook-Air.local>
2026-05-06 15:16:22 +0900

    two code change

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/class-portfolio/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
